6af9fbcd63bf5f828b0afe67883cf901b8f2dea1
Maik Macho <nuda1998@gmail.com>
2016-06-24 16:22:16 +0200

    Simplify build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-cd Assembly-CSharp.ExampleAPI.mm
-if [ -z ${BUILD_NUMBER+x} ]; then
-  echo 'build_main: Leaving version default.'
-else
-  echo 'build_main: Replacing version string in FEZMod.'
-  perl -0777 -pi -e 's/public string Version { get { return ".*"; } }/public string Version { get { return "dev-'${BUILD_NUMBER}'"; } }/gm' ./src/ExampleMod.cs
-fi
-xbuild && \
-cd ..
+xbuild
