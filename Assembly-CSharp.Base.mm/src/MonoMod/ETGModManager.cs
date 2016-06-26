d227096b4d6fd8062180ac8eaf99156d207a9ae8
ZusMonolith <thevisitorsmonolith@gmail.com>
2016-06-26 17:59:09 -0500

    Changed the way the mods work, there's now a Monobehaviour that manages it all, instead of modifying GameManager lots.

---
+++ Assembly-CSharp.Base.mm/src/MonoMod/ETGModManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+//This class is responsible for all mod managment 
+class ETGModManager : MonoBehaviour{
+
+    public void Awake() {
+
+    }
+
+    public void Start() {
+        ETGMod.Start();
+
+    }
+
+    public void Update() {
+        ETGMod.Update();
+    }
+
+    //This is for GUI stuff.
+    public void OnGUI() {
+        if(patch_GameManager.GameManager_0)
+            if (patch_GameManager.GameManager_0.PlayerController_1!=null)
+                GUILayout.Label(patch_GameManager.GameManager_0.PlayerController_1.transform.position.ToString());
+    }
+
+}
--- Assembly-CSharp.Base.mm/src/patch_GameManager.cs
+++ Assembly-CSharp.Base.mm/src/patch_GameManager.cs
@@ -13,26 +13,16 @@ public class patch_GameManager : GameManager {
         return orig_get_GameObject_1();
     }
 
+    public static GameObject ModManagerObject;
+
     protected extern void orig_Awake();
     private void Awake() {
         orig_Awake();
 
-        // Gets called more than once in the game's lifetime...
-        ETGMod.Start();
-    }
+        GameObject ModManagerObject = new GameObject();
 
-    protected extern void orig_Update();
-    protected new void Update() {
-        orig_Update();
-
-        ETGMod.Update();
-    }
-
-
-    //This is for GUI stuff.
-    public void OnGUI() {
-        if (this.PlayerController_1!=null)
-            GUILayout.Label(this.PlayerController_1.transform.position.ToString());
+        ModManagerObject.name="ModManager";
+        ModManagerObject.AddComponent<ETGModManager>();
     }
 
 }
