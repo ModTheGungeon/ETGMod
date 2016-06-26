fc094a593675dd8a58126d8c8c7fd8cb3e6ff1d4
Maik Macho <nuda1998@gmail.com>
2016-06-26 16:05:40 +0200

    Allow CharacterSelectController to stringify non-hardcoded (new?) characters

---
+++ Assembly-CSharp.Base.mm/src/patch_CharacterSelectController.cs
@@ -0,0 +1,31 @@
+﻿#pragma warning disable 0626
+#pragma warning disable 0649
+
+using UnityEngine;
+
+public class patch_CharacterSelectController : CharacterSelectController {
+
+    /// <summary>
+    /// Stringify the character so it can be used "elsewhere".
+    /// </summary>
+    /// <param name="character">Character to stringify.</param>
+    /// <returns>"Player" + character.ToString()</returns>
+    public static new string smethod_0(PlayableCharacters character) {
+        return "Player" + character.ToString();
+    }
+
+    public static extern string orig_smethod_1();
+    /// <summary>
+    /// Get quick start character.
+    /// </summary>
+    /// <returns>Preferred quick start character (last used or configured).</returns>
+    public static new string smethod_1() {
+        if (GameManager.GameOptions_0.PreferredQuickstartCharacter == GameOptions.QuickstartCharacter.LAST_USED) {
+            return smethod_0(GameManager.GameOptions_0.LastPlayedCharacter);
+        }
+
+        // The GameOptions.QuickstartCharacter enum names are uppercase - let's just fall back to the original hardcoded values.
+        return orig_smethod_1();
+    }
+
+}
