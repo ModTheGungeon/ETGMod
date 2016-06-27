066421411a409aa1fafaba6697cf8c9c465e8a41
Maik Macho <nuda1998@gmail.com>
2016-06-27 21:49:45 +0200

    Make use of bleeding-edge MonoMod functionality

--- UnityEngine.Base.mm/src/patch_Resources.cs
+++ UnityEngine.Base.mm/src/patch_Resources.cs
@@ -2,68 +2,75 @@
 using System.IO;
 using System.Runtime.CompilerServices;
 using UnityEngineInternal;
+using MonoMod;
 
 namespace UnityEngine {
     public sealed class patch_Resources {
 
         [/*WrapperlessIcall, */TypeInferenceRule(TypeInferenceRules.TypeReferencedBySecondArgument)]
         [MethodImpl(MethodImplOptions.InternalCall)]
-        public static extern Object orig_Load(string path, System.Type systemTypeInstance);
+        [MonoModOriginal] public static extern Object Ooad(string path, System.Type systemTypeInstance);
+        [MonoModOriginalName("Ooad")]
         public static Object Load(string path, System.Type type) {
             Log("HOOKED LOAD");
             Log(System.Environment.StackTrace.ToString());
 
-            return orig_Load(path, type);
+            return Ooad(path, type);
         }
 
         //[WrapperlessIcall]
         [MethodImpl(MethodImplOptions.InternalCall)]
-        public static extern ResourceRequest orig_LoadAsync(string path, System.Type type);
+        [MonoModOriginal] public static extern ResourceRequest OoadAsync(string path, System.Type type);
+        [MonoModOriginalName("OoadAsync")]
         public static ResourceRequest LoadAsync(string path, System.Type type) {
             Log("HOOKED WHAT, LOADASYNC");
             Log(System.Environment.StackTrace.ToString());
 
-            return orig_LoadAsync(path, type);
+            return OoadAsync(path, type);
         }
 
         //[WrapperlessIcall]
         [MethodImpl(MethodImplOptions.InternalCall)]
-        public static extern Object[] orig_LoadAll(string path, System.Type systemTypeInstance);
+        [MonoModOriginal] public static extern Object[] OoadAll(string path, System.Type systemTypeInstance);
+        [MonoModOriginalName("OoadAll")]
         public static Object[] LoadAll(string path, System.Type type) {
             Log("HOOKED WHY, LOADALL");
             Log(System.Environment.StackTrace.ToString());
 
-            return orig_LoadAll(path, type);
+            return OoadAll(path, type);
         }
 
         [/*WrapperlessIcall,*/ TypeInferenceRule(TypeInferenceRules.TypeReferencedByFirstArgument)]
         [MethodImpl(MethodImplOptions.InternalCall)]
-        public static extern Object orig_GetBuiltinResource(System.Type type, string path);
+        [MonoModOriginal] public static extern Object OetBuiltinResource(System.Type type, string path);
+        [MonoModOriginalName("OetBuiltinResource")]
         public static Object GetBuiltinResource(System.Type type, string path) {
             Log("HOOKED HOW, GETBUILTINRESOURCE");
             Log(System.Environment.StackTrace.ToString());
 
-            return orig_GetBuiltinResource(type, path);
+            return OetBuiltinResource(type, path);
         }
 
         //[WrapperlessIcall]
         [MethodImpl(MethodImplOptions.InternalCall)]
-        public static extern void orig_UnloadAsset(Object assetToUnload);
+        [MonoModOriginal] public static extern void OnloadAsset(Object assetToUnload);
+        [MonoModOriginalName("OnloadAsset")]
         public static void UnloadAsset(Object asset) {
             Log("HOOKED WHEN, UNLOADASSET");
             Log(System.Environment.StackTrace.ToString());
 
-            orig_UnloadAsset(asset);
+            OnloadAsset(asset);
         }
 
         //[WrapperlessIcall]
         [MethodImpl(MethodImplOptions.InternalCall)]
-        public static extern AsyncOperation orig_UnloadUnusedAssets();
+        [MonoModOriginal] public static extern AsyncOperation OnloadUnusedAssets();
+        [MonoModOriginalName("OnloadUnusedAssets")]
         public static AsyncOperation UnloadUnusedAssets() {
             Log("HOOKED WHATEVER, UNLOADUNUSEDASSETS");
             Log(System.Environment.StackTrace.ToString());
 
-            return orig_UnloadUnusedAssets();
+            return OnloadUnusedAssets();
         }
 
         private static void Log(string text) {
