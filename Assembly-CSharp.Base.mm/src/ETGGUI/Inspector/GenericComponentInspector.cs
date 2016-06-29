3bffa6639d2efb16c941ce1d3fa90fba1da3dabf
ZusMonolith <thevisitorsmonolith@gmail.com>
2016-06-29 12:16:44 -0500

    OOPS. Working now!

---
+++ Assembly-CSharp.Base.mm/src/ETGGUI/Inspector/GenericComponentInspector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace src.ETGGUI.Inspector {
+    class GenericComponentInspector {
+
+        public void OnGUI(object instance) {
+
+            PropertyInfo[] allProperties = instance.GetType().GetProperties();
+
+            foreach (PropertyInfo inf in allProperties) {
+                if (inf.MemberType==MemberTypes.Method)
+                    continue;
+                //object getProperty = ReflectionHelper.GetValue(inf, instance);
+
+
+                GUILayout.Label(inf.ToString());
+            }
+
+        }
+
+    }
+}
