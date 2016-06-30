1029b32ee333e3f443ce9b9d465a85fc08cd2bb5
Maik Macho <nuda1998@gmail.com>
2016-06-30 15:30:17 +0200

    More ordered inspector

--- Assembly-CSharp.Base.mm/src/ETGGUI/Inspector/Components/GenericComponentInspector.cs
+++ Assembly-CSharp.Base.mm/src/ETGGUI/Inspector/Components/GenericComponentInspector.cs
@@ -9,21 +9,52 @@ namespace ETGGUI.Inspector {
     public class GenericComponentInspector {
 
         public static List<string> PropertyBlacklist = new List<string>() {
+            "UnityEngine.Object::name",
+            "UnityEngine.Component::tag",
             "dfControl::InverseClipChildren"
         };
 
-        public void OnGUI(object instance) {
+        private static PropertyInfo p_Object_name = typeof(UnityEngine.Object).GetProperty("name");
+        private static PropertyInfo p_Transform_tag = typeof(Transform).GetProperty("tag");
 
-            PropertyInfo[] allProperties = instance.GetType().GetProperties();
+        private List<string> crawled = new List<string>();
+        public void OnGUI(object instance) {
+            crawled.Clear();
+            crawled.Add("name");
+            crawled.Add("tag");
+            PropertyInfo[] allProperties = instance.GetType().GetProperties(
+                BindingFlags.Public |
+                BindingFlags.Instance |
+                BindingFlags.DeclaredOnly
+            );
+
+            if (instance is Transform) {
+                object getProperty = ((Transform) instance).name;
+                object setProperty = ETGModInspector.DrawProperty(p_Object_name, getProperty);
+                if (getProperty != setProperty) {
+                    ((Transform) instance).name = (string) setProperty;
+                }
+
+                getProperty = ((Transform) instance).tag;
+                setProperty = ETGModInspector.DrawProperty(p_Transform_tag, getProperty);
+                if (getProperty != setProperty) {
+                    ((Transform) instance).tag = (string) setProperty;
+                }
+            }
+
+            GUILayout.Label("");
+            GUILayout.Label("Component: " + instance.GetType().FullName);
 
             foreach (PropertyInfo inf in allProperties) {
                 string fullName = inf.DeclaringType.FullName + "::" + inf.Name;
                 if (PropertyBlacklist.Contains(fullName) ||
+                    crawled.Contains(fullName) ||
                     inf.MemberType == MemberTypes.Method ||
                     inf.GetIndexParameters().Length != 0 ||
                     !inf.CanRead || !inf.CanWrite) {
                     continue;
                 }
+                crawled.Add(inf.Name);
 
                 try {
                     object getProperty = ReflectionHelper.GetValue(inf, instance);
