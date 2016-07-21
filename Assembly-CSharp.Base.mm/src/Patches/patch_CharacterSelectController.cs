f948609b159bcd59eef08116c20541c996bb5359
Maik Macho <nuda1998@gmail.com>
2016-07-21 22:59:50 +0200

    Fix CharacterSelectController

--- Assembly-CSharp.Base.mm/src/Patches/patch_CharacterSelectController.cs
+++ Assembly-CSharp.Base.mm/src/Patches/patch_CharacterSelectController.cs
@@ -5,37 +5,28 @@ using UnityEngine;
 
 internal class patch_CharacterSelectController : CharacterSelectController {
 
-    /// <summary>
-    /// Stringify the character so it can be used "elsewhere".
-    /// </summary>
-    /// <param name="character">Character to stringify.</param>
-    /// <returns>"Player" + character.ToString()</returns>
-    public static new string smethod_0(PlayableCharacters character) {
+    public static new string GetCharacterPathFromIdentity(PlayableCharacters character) {
         if (character == PlayableCharacters.Pilot) {
             return "PlayerRogue";
         }
-        return "Player" + character.ToString();
+        return "Player" + character;
     }
 
-    public static extern string orig_smethod_1();
-    /// <summary>
-    /// Get quick start character.
-    /// </summary>
-    /// <returns>Preferred quick start character (last used or configured).</returns>
-    public static new string smethod_1() {
+    public static extern string orig_GetCharacterPathFromQuickStart();
+    public static new string GetCharacterPathFromQuickStart() {
         if (ETGMod.Player.QuickstartReplacement != null) {
             return ETGMod.Player.QuickstartReplacement;
         }
 
-        if (GameManager.GameOptions_0.PreferredQuickstartCharacter == GameOptions.QuickstartCharacter.LAST_USED) {
-            if (GameManager.GameOptions_0.LastPlayedCharacter == PlayableCharacters.Pilot) {
+        if (GameManager.Options.PreferredQuickstartCharacter == GameOptions.QuickstartCharacter.LAST_USED) {
+            if (GameManager.Options.LastPlayedCharacter == PlayableCharacters.Pilot) {
                 return "PlayerRogue";
             }
-            return smethod_0(GameManager.GameOptions_0.LastPlayedCharacter);
+            return GetCharacterPathFromIdentity(GameManager.Options.LastPlayedCharacter);
         }
 
         // The GameOptions.QuickstartCharacter enum names are uppercase - let's just fall back to the original hardcoded values.
-        return orig_smethod_1();
+        return orig_GetCharacterPathFromQuickStart();
     }
 
 }
