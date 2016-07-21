92c39b11fbe573de753fce6b2dd9d722e90b49dc
Maik Macho <nuda1998@gmail.com>
2016-07-21 23:05:26 +0200

    Fix GameManager

--- Assembly-CSharp.Base.mm/src/Patches/patch_GameManager.cs
+++ Assembly-CSharp.Base.mm/src/Patches/patch_GameManager.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 
 internal class patch_GameManager : GameManager {
 
-    public static extern GameObject orig_get_GameObject_1();
-    public static GameObject get_GameObject_1() {
+    public static extern GameObject orig_get_CoopPlayerPrefabForNewGame();
+    public static GameObject get_CoopPlayerPrefabForNewGame() {
         // Doesn't actually replace the Cultist
         if (ETGMod.Player.CoopReplacement != null) {
             return Resources.Load(ETGMod.Player.CoopReplacement) as GameObject;
         }
-        return orig_get_GameObject_1();
+        return orig_get_CoopPlayerPrefabForNewGame();
     }
 
     protected extern void orig_Awake();
