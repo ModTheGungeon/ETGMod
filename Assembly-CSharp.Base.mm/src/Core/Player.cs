6a35ffcacd23ec2f2aaef33ed76c90a139c7be14
Maik Macho <nuda1998@gmail.com>
2016-07-21 23:41:16 +0200

    Make GiveItemID work again, without Cross

--- Assembly-CSharp.Base.mm/src/Core/Player.cs
+++ Assembly-CSharp.Base.mm/src/Core/Player.cs
@@ -12,23 +12,12 @@ public static partial class ETGMod {
     /// ETGMod player configuration.
     /// </summary>
     public static class Player {
-        public readonly static MethodInfo _GiveItem = new CrossSearch<MethodInfo>() {
-            Name = "smethod_15",
-            In = "GClass200", // Linux
-            Public = true,
-            Static = true,
-            Returns = typeof(bool),
-            Args = new [] { typeof(GameObject), typeof(PlayerController), typeof(bool) }
-            // No context required.
-        }.Find();
         public static bool GiveItemID(int id) {
             if (!GameManager.Instance.PrimaryPlayer) {
                 Debug.Log ("Couldn't access PlayerController instance in GameManager");
                 return false;
             }
-            PlayerController playercontroller = GameManager.Instance.PrimaryPlayer;
-            GameObject pickupobject = PickupObjectDatabase.GetById (id).gameObject;
-            _GiveItem.Xs(pickupobject, playercontroller, false);
+            LootEngine.TryGivePrefabToPlayer(PickupObjectDatabase.GetById(id).gameObject, GameManager.Instance.PrimaryPlayer, false);
             return true;
         }
 
