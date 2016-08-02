2be6be8c3ddf88e3e62a62bf949c3af0fdeb2bcd
Maik Macho <nuda1998@gmail.com>
2016-08-02 22:38:18 +0200

    Completely remove DUMMY

--- DUMMY/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace DUMMY {
-	class Program {
-		public static void Main(string[] args) {
-			Console.WriteLine("This project only exists to make MonoDevelop show \"Custom Command Mono Soft Debugger\".");
-		}
-	}
-}
