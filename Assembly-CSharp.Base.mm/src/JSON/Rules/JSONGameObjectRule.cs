f6374870d2d0802771b6e31f7b23e5978e26fb43
Maik Macho <nuda1998@gmail.com>
2016-08-09 14:31:33 +0200

    Change how GameObjects get serialized; Outside component references still broken

--- Assembly-CSharp.Base.mm/src/JSON/Rules/JSONGameObjectRule.cs
+++ Assembly-CSharp.Base.mm/src/JSON/Rules/JSONGameObjectRule.cs
@@ -9,22 +9,26 @@ using System.Collections;
 
 public class JSONGameObjectRule : JSONRule<GameObject> {
 
-    // Add components (types)
-    // Add children
-    // Fill components (from bottom to top)
-
-    // Issue:
-    // Child 1 refers to component A of 
-
     public override void Serialize(JsonHelperWriter json, object obj) {
         GameObject go = (GameObject) obj;
-        Component[] components = go.GetComponents<Component>();
 
+        SerializeMain(json, go);
+        SerializeComponentTypes(json, go);
+        SerializeHierarchy(json, go);
+
+        SerializeComponentData(json, go);
+    }
+
+    public void SerializeMain(JsonHelperWriter json, GameObject go) {
         json.WriteProperty("name", go.name);
 
         json.WriteProperty("tag", go.tag);
         json.WriteProperty("layer", go.layer);
         json.WriteProperty("activeSelf", go.activeSelf);
+    }
+
+    public void SerializeComponentTypes(JsonHelperWriter json, GameObject go) {
+        Component[] components = go.GetComponents<Component>();
 
         json.WritePropertyName("componentTypes");
         json.WriteStartArray();
@@ -32,37 +36,77 @@ public class JSONGameObjectRule : JSONRule<GameObject> {
             json.Write(components[i].GetType());
         }
         json.WriteEndArray();
+    }
+
+    public void SerializeHierarchy(JsonHelperWriter json, GameObject go) {
+        Transform transform = go.transform;
+        int children = transform.childCount;
+
+        json.WritePropertyName("hierarchy");
+        json.WriteStartArray();
+        for (int i = 0; i < children; i++) {
+            GameObject child = transform.GetChild(i).gameObject;
+            json.WriteStartObject();
+            SerializeMain(json, child);
+            SerializeComponentTypes(json, child);
+            SerializeHierarchy(json, child);
+            json.WriteEndObject();
+        }
+        json.WriteEndArray();
+    }
+
+    public void SerializeComponentData(JsonHelperWriter json, GameObject go, bool property = true) {
+        Component[] components = go.GetComponents<Component>();
 
         Transform transform = go.transform;
         int children = transform.childCount;
-        json.WritePropertyName("children");
-        json.WriteStartArray();
-        for (int i = 0; i < children; i++) {
-            json.Write(transform.GetChild(i).gameObject);
-        }
-        json.WriteEndArray();
 
-        json.WritePropertyName("components");
+        if (property) {
+            json.WritePropertyName("componentData");
+        }
         json.WriteStartArray();
+
+        json.WriteValue(components.Length);
         for (int i = 0; i < components.Length; i++) {
             json.Write(components[i]);
         }
+
+        json.WriteStartArray();
+        for (int i = 0; i < children; i++) {
+            GameObject child = transform.GetChild(i).gameObject;
+            SerializeComponentData(json, child, false);
+        }
+        json.WriteEndArray();
+
         json.WriteEndArray();
     }
 
     public override object Deserialize(JsonHelperReader json, object obj) {
         GameObject go = (GameObject) obj;
 
+        bool active = DeserializeMain(json, go);
+        go.SetActive(false);
+        DeserializeComponentTypes(json, go);
+        DeserializeHierarchy(json, go);
+
+        DeserializeComponentData(json, go);
+
+        go.SetActive(active);
+        return go;
+    }
+
+    public bool DeserializeMain(JsonHelperReader json, GameObject go) {
         go.name = (string) json.ReadRawProperty("name");
 
         json.Global[go.name] = go;
         json.Global[go.transform.GetPath()] = go;
-        
+
         go.tag = (string) json.ReadRawProperty("tag");
         go.layer = (int) (long) json.ReadRawProperty("layer");
-        bool active = (bool) json.ReadRawProperty("activeSelf");
-        go.SetActive(false);
+        return (bool) json.ReadRawProperty("activeSelf");
+    }
 
+    public void DeserializeComponentTypes(JsonHelperReader json, GameObject go) {
         json.ReadPropertyName("componentTypes");
         json.Read(); // Drop StartArray
         while (json.TokenType != JsonToken.EndArray) {
@@ -72,26 +116,47 @@ public class JSONGameObjectRule : JSONRule<GameObject> {
             }
         }
         json.Read(); // Drop EndArray
+    }
 
-        json.ReadPropertyName("children");
+    public void DeserializeHierarchy(JsonHelperReader json, GameObject go) {
+        json.ReadPropertyName("hierarchy");
         Transform transform = go.transform;
         json.Read(); // Drop StartArray
         while (json.TokenType != JsonToken.EndArray) {
             GameObject child = new GameObject();
             child.transform.SetParent(transform);
-            json.FillObject(child);
+            json.Read(); // Drop StartObject
+            DeserializeMain(json, child);
+            DeserializeComponentTypes(json, child);
+            DeserializeHierarchy(json, child);
+            json.Read(); // Drop EndObject
         }
         json.Read(); // Drop EndArray
+    }
 
-        json.ReadPropertyName("components");
+    public void DeserializeComponentData(JsonHelperReader json, GameObject go, bool property = true) {
+        Transform transform = go.transform;
+        int children = transform.childCount;
+
+        if (property) {
+            json.ReadPropertyName("componentData");
+        }
         json.Read(); // Drop StartArray
-        while (json.TokenType != JsonToken.EndArray) {
+
+        int components = (int) (long) json.Value; json.Read();
+        for (int i = 0; i < components; i++) {
             FillComponent(json, go);
         }
+
+        json.Read(); // Drop StartArray
+        int ii = -1;
+        while (json.TokenType != JsonToken.EndArray) {
+            GameObject child = transform.GetChild(++ii).gameObject;
+            DeserializeComponentData(json, child, false);
+        }
         json.Read(); // Drop EndArray
 
-        go.SetActive(active);
-        return go;
+        json.Read(); // Drop EndArray
     }
 
     public void FillComponent(JsonHelperReader json, GameObject go) {
