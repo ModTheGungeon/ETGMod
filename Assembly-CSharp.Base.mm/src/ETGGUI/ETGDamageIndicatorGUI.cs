03bef46ac9f9aa3927886ce0b29df031571373eb
ZusMonolith <thevisitorsmonolith@gmail.com>
2016-08-19 17:01:18 -0500

    Re-did damage indicators.

--- Assembly-CSharp.Base.mm/src/ETGGUI/ETGDamageIndicatorGUI.cs
+++ Assembly-CSharp.Base.mm/src/ETGGUI/ETGDamageIndicatorGUI.cs
@@ -6,17 +6,10 @@ using UnityEngine;
 
 class ETGDamageIndicatorGUI : MonoBehaviour {
 
-    private static List<DamageIndicator> _Indicators = new List<DamageIndicator>();
-    private static List<DamageIndicator> _UsedPool = new List<DamageIndicator>();
-    private static List<DamageIndicator> _ToRemove = new List<DamageIndicator>();
-
-    private static Dictionary<HealthHaver, HealthBar> _AllBars = new Dictionary<HealthHaver, HealthBar>();
-    public static Dictionary<HealthHaver, float> MaxHP = new Dictionary<HealthHaver, float>();
-    public static Dictionary<HealthHaver, float> CurrentHP = new Dictionary<HealthHaver, float>();
-
-    public static List<HealthHaver> ToRemoveBars = new List<HealthHaver>();
+    private static Dictionary<HealthHaver, HealthHaverTracker> _AllBars = new Dictionary<HealthHaver, HealthHaverTracker>();
 
     public static bool RenderHealthBars = true;
+    public static bool RenderIndicators = true;
 
     public static void Create() {
         GameObject newObject = new GameObject();
@@ -30,171 +23,92 @@ class ETGDamageIndicatorGUI : MonoBehaviour {
     }
 
     public void Update() {
-        foreach (DamageIndicator i in _Indicators)
-            i.Update();
 
-        foreach (DamageIndicator i in _ToRemove)
-            _Indicators.Remove(i);
+        foreach (DamageIndicator d in DamageIndicator.allIndicators)
+            d.Update();
 
-        try {
-            foreach (HealthBar bar in _AllBars.Values)
-                bar.Update();
+        foreach (DamageIndicator d in DamageIndicator.removeIndicators)
+            DamageIndicator.allIndicators.Remove(d);
 
-            foreach (HealthHaver hh in ToRemoveBars) {
-                _AllBars.Remove(hh);
-            }
+        DamageIndicator.removeIndicators.Clear();
 
-            _ToRemove.Clear();
-            ToRemoveBars.Clear();
-        }
-        catch (System.Exception e) {
-            Debug.Log(e.ToString());
-        }
     }
 
     public void OnGUI() {
-        foreach (DamageIndicator i in _Indicators)
-            i.OnGUI();
 
-        foreach (HealthBar bar in _AllBars.Values)
-            bar.OnGUI();
+        foreach (DamageIndicator d in DamageIndicator.allIndicators)
+            d.OnGUI();
+
     }
 
-    public static void CreateIndicator(Vector3 worldPosOrigin, object content) {
+    public static void HealthHaverTookDamage(HealthHaver dmg, float damage) {
 
-        //We need to make a new object
-        if (_UsedPool.Count==0) {
-            DamageIndicator newIndicator = new DamageIndicator();
+        if (!_AllBars.ContainsKey(dmg)) {
+            HealthHaverTracker tracker = new HealthHaverTracker();
 
-            newIndicator.WPosOrigin=worldPosOrigin;
-            newIndicator.Content="<size=35>"+content+"</size>";
+            tracker.thisHaver=dmg;
+            _AllBars.Add(dmg,tracker);
+        }
 
-            _Indicators.Add(newIndicator);
-        } else {
-            //We have a pooled object, use this instead
-
-            DamageIndicator pickedIndicator = _UsedPool[0];
-            _UsedPool.RemoveAt(0);
-
-            pickedIndicator.Content="<size=35>"+content+"</size>";
-            pickedIndicator.WPosOrigin=worldPosOrigin;
-
-            _Indicators.Add(pickedIndicator);
+        if (RenderIndicators) {
+            DamageIndicator newIndicator = new DamageIndicator(dmg.specRigidbody.UnitCenter,damage);
         }
     }
 
-    public static void CreateBar(HealthHaver targ) {
+    class HealthHaverTracker {
+        public HealthHaver thisHaver;
 
-        if (_AllBars.ContainsKey(targ))
-            return;
 
-        HealthBar newBar = new HealthBar();
-        newBar.Target=targ;
 
-        _AllBars.Add(targ, newBar);
     }
 
-    public static void UpdateHealthBar(HealthHaver hh, float dmg) {
-        if (_AllBars.ContainsKey(hh))
-            _AllBars[hh].UpdateTransitionBar(dmg);
+    class HealthBar {
+        public HealthHaverTracker tracker;
+
+
     }
 
-    private class DamageIndicator {
-        public Vector2 Offset;
-        public Vector3 WPosOrigin;
-        public object Content;
+    class DamageIndicator {
 
-        private float _Time;
+        Vector3 position = Vector3.zero;
+        float damageTaken = 0;
+        float wiggleAmount = 0;
+        float lifetime = 1;
+        float speed = 1;
+
+        public static List<DamageIndicator> allIndicators = new List<DamageIndicator>();
+        public static List<DamageIndicator> removeIndicators = new List<DamageIndicator>();
+
+        public DamageIndicator(Vector3 wPosStart, float damageTaken = 0, float wiggleAmount = 0, float lifetime = 1, float speed = 1) {
+            this.position=wPosStart;
+            this.damageTaken=damageTaken;
+            this.wiggleAmount=wiggleAmount;
+            this.lifetime=lifetime;
+            this.speed=speed;
+
+            allIndicators.Add(this);
+        }
 
         public void Update() {
-            Offset-=Vector2.up*Time.deltaTime*5;
-            Offset=new Vector2(Mathf.Sin(_Time*15)-0.5f, Offset.y);
-            _Time+=Time.deltaTime;
 
-            if (_Time>=2) {
-                _UsedPool.Add(this);
-                _ToRemove.Add(this);
-                _Time=0;
-                Offset=Vector2.zero;
-            }
+            position+=Vector3.up*speed*Time.deltaTime;
+
+            lifetime-=Time.deltaTime;
+            if (lifetime<=0)
+                Destroy();
         }
 
         public void OnGUI() {
-            Vector2 worldToScreenPoint = Camera.main.WorldToScreenPoint(WPosOrigin);
+
+            Vector2 worldToScreenPoint = Camera.main.WorldToScreenPoint(position);
             worldToScreenPoint=new Vector2(worldToScreenPoint.x, Screen.height-worldToScreenPoint.y);
-            GUI.color=Color.red;
-            GUI.Label(new Rect(worldToScreenPoint+( Offset*15 )+( new Vector2(25, 25) ), new Vector2(50, 50)), Content.ToStringIfNoString());
-            GUI.color=Color.white;
-        }
-    }
 
-    private class HealthBar {
-        public HealthHaver Target;
-
-        Rect OutlineRect = new Rect();
-        Rect TotalBarRect = new Rect();
-        Rect FilledBarRect = new Rect();
-        Rect TransitionBarRect = new Rect();
-
-        //The point on the HP bar we're transitioning to.
-        float CurrentPoint = 1f;
-        //The time before transition takes place.
-        float TransitionDelay = 0.1f;
-        float TransitionSpeed = 0.3f;
-
-        public void Update() {
-
-            if (Target==null) {
-                ToRemoveBars.Add(Target);
-                return;
-            }
-
-            if (TransitionDelay>0)
-                TransitionDelay-=Time.deltaTime;
-            else if (CurrentPoint>=CurrentHP[Target]/MaxHP[Target])
-                CurrentPoint-=Time.deltaTime*TransitionSpeed;
+            GUI.Label(new Rect(worldToScreenPoint,Vector2.one*250), "<size=30> " + damageTaken.ToString() + " </size>");
 
         }
 
-        public void UpdateTransitionBar(float damageDelta) {
-
-            //Local hit point, the place we where at before we took damage
-            float hitPointL = ( CurrentHP[Target]+damageDelta )/MaxHP[Target];
-
-            //If the current hit point is greater than the local one, we'll leave it as-is. Otherwise we're going to set it to the current hit point.
-            if (Mathf.Abs(CurrentPoint-hitPointL)<0.01f) {
-                CurrentPoint=hitPointL;
-            }
-
-            TransitionDelay=0.1f;
-        }
-
-        public void OnGUI() {
-
-            try {
-                Vector3 wPos = (Vector3)Target.specRigidbody.PrimaryPixelCollider.UnitTopCenter;
-                Vector2 screenPos = Camera.main.WorldToScreenPoint(wPos);
-                screenPos=new Vector2(screenPos.x, Screen.height-screenPos.y);
-
-                int hpMaxBar = (int)Mathf.Max(0, 15-( MaxHP[Target] ))+100;
-
-                OutlineRect=new Rect(screenPos.x-( hpMaxBar/2 )-2, screenPos.y-12, hpMaxBar+4, 24);
-                TotalBarRect=new Rect(screenPos.x-( hpMaxBar/2 ), screenPos.y-10, hpMaxBar, 20);
-                FilledBarRect=new Rect(screenPos.x-( hpMaxBar/2 ), screenPos.y-10, hpMaxBar*( CurrentHP[Target]/MaxHP[Target] ), 20);
-                TransitionBarRect=new Rect(screenPos.x-( hpMaxBar/2 ), screenPos.y-10, hpMaxBar*( CurrentPoint ), 20);
-
-                GUI.color=new Color(0, 0, 0, 1);
-                GUI.DrawTexture(OutlineRect, ETGModGUI.BoxTexture);
-                GUI.color=new Color(151f/255f, 166f/255f, 170f/255f);
-                GUI.DrawTexture(TotalBarRect, ETGModGUI.BoxTexture);
-                GUI.color=Color.Lerp(new Color(229f/255f, 54f/255f, 23f/255f), new Color(77f/255f, 214f/255f, 80f/255f), CurrentHP[Target]/MaxHP[Target]);
-                GUI.DrawTexture(FilledBarRect, ETGModGUI.BoxTexture);
-                GUI.color=Color.Lerp(new Color(229f/255f, 54f/255f, 23f/255f), new Color(77f/255f, 214f/255f, 80f/255f), CurrentPoint)*0.5f;
-                GUI.DrawTexture(TransitionBarRect, ETGModGUI.BoxTexture);
-                GUI.color=Color.white;
-            } catch {
-
-            }
+        public void Destroy() {
+            removeIndicators.Add(this);
         }
 
     }
--- Assembly-CSharp.Base.mm/src/Patches/patch_HealthHaver.cs
+++ Assembly-CSharp.Base.mm/src/Patches/patch_HealthHaver.cs
@@ -13,26 +13,10 @@ internal class patch_HealthHaver : HealthHaver {
     protected extern void orig_ApplyDamage(float damage, Vector2 direction, string sourceName, CoreDamageTypes damageTypes = CoreDamageTypes.None, DamageCategory damageCategory = DamageCategory.Normal, bool ignoreInvulnerabilityFrames = false, PixelCollider hitPixelCollider = null, bool ignoreDamageCaps = false);
     protected void ApplyDamage(float damage, Vector2 direction, string sourceName, CoreDamageTypes damageTypes = CoreDamageTypes.None, DamageCategory damageCategory = DamageCategory.Normal, bool ignoreInvulnerabilityFrames = false, PixelCollider hitPixelCollider = null, bool ignoreDamageCaps = false) {
         orig_ApplyDamage(damage, direction, sourceName, damageTypes, damageCategory, ignoreInvulnerabilityFrames, hitPixelCollider, ignoreDamageCaps);
-        if (currentHealth == 0f) {
+        if (currentHealth == 0f || damage<=0) 
             return;
-        }
 
-        Vector3 centerPos = specRigidbody.UnitTopCenter;
-
-        if (ETGModGUI.UseDamageIndicators) {
-            ETGDamageIndicatorGUI.CreateIndicator(centerPos, damage);
-            ETGDamageIndicatorGUI.CreateBar(this);
-        }
-        ETGDamageIndicatorGUI.MaxHP[this] = maximumHealth;
-        ETGDamageIndicatorGUI.CurrentHP[this] = currentHealth;
-
-        ETGDamageIndicatorGUI.UpdateHealthBar(this, damage);
-
-        if (currentHealth == 0) {
-            ETGDamageIndicatorGUI.ToRemoveBars.Add(this);
-            ETGDamageIndicatorGUI.MaxHP.Remove(this);
-            ETGDamageIndicatorGUI.CurrentHP.Remove(this);
-        }
+        ETGDamageIndicatorGUI.HealthHaverTookDamage(this, damage);
     }
 
 }
