283e17afad931a1093d220d4c01cb2d360224e5b
ZusMonolith <thevisitorsmonolith@gmail.com>
2016-08-25 18:09:43 -0500

    Pushing NON-FUNCTIONAL OUTPUT LOG so 0x0ade can fix it maybe later

--- Assembly-CSharp.Base.mm/src/ETGGUI/ETGModDebugLogMenu.cs
+++ Assembly-CSharp.Base.mm/src/ETGGUI/ETGModDebugLogMenu.cs
@@ -5,70 +5,31 @@ using UnityEngine;
 using System.Collections.Generic;
 using SGUI;
 
-public class ETGModDebugLogMenu : IETGModMenu {
+public class ETGModDebugLogMenu : ETGModMenu {
 
     /// <summary>
     /// All debug logged text lines. Feel free to add your lines here!
     /// </summary>
     private static List<LoggedText> allLoggedText = new List<LoggedText>();
 
-    public static Vector2 ScrollPos;
+    public static ETGModDebugLogMenu Instance;
 
-    private static Rect _MainBoxRect = new Rect(16, 16, Screen.width-32, Screen.height-32);
+    public override void Start() {
 
-    public SGroup GUI { get; protected set; }
+        GUI=new SGroup {
+            Visible=false,
+            OnUpdateStyle=(SElement elem) => elem.Fill(),
+            AutoLayout=(SGroup g) => g.AutoLayoutRows,
+            ScrollDirection=SGroup.EDirection.Vertical,
+            Children= {
+                        new SLabel("THIS LOG IS <color=#ff0000ff>WORK IN PROGRESS</color>."),
+                        new SLabel("It drops the Unity OnGUI / IMGUI system for SGUI."),
+                        new SLabel("Some code may still be missing in the SGUI port."),
+                        new SLabel()
+                    }
+        };
 
-    public void Start() {
-    }
-
-    public void Update() {
-
-    }
-
-    public void OnGUI() {
-
-        //Set rect
-        _MainBoxRect = new Rect(16, 16, Screen.width - 32, Screen.height - 32);
-
-        //Draw main box
-        _DrawMainBox();
-
-        //Draw the logged text
-        _DrawLoggedText();
-    }
-
-    public void OnOpen() { }
-
-    public void OnClose() { }
-
-    public void OnDestroy() { }
-
-    private void _DrawMainBox() {
-        UnityEngine.GUI.Box(_MainBoxRect, string.Empty);
-    }
-
-    private void _DrawLoggedText() {
-
-        GUILayout.BeginArea(_MainBoxRect);
-
-        ScrollPos=GUILayout.BeginScrollView(ScrollPos);
-
-        foreach ( LoggedText msg in allLoggedText) {
-            GUILayout.BeginHorizontal();
-            if (GUILayout.Button("", GUILayout.Width(20)))
-                msg.IsStacktraceShown=!msg.IsStacktraceShown;
-            GUILayout.Label(( msg.LogCount>0 ? "("+ msg.LogCount + ")" : "") + msg.LogMessage);
-            GUILayout.EndHorizontal();
-
-            if (msg.IsStacktraceShown) {
-                GUILayout.Label("----Stacktrace---- \n" + "    " + msg.Stacktace);
-            }
-
-            GUILayout.Space(3);
-        }
-
-        GUILayout.EndScrollView();
-        GUILayout.EndArea();
+        Instance=this;
 
     }
 
@@ -86,28 +47,69 @@ public class ETGModDebugLogMenu : IETGModMenu {
 
     public static void Logger(string text, string stackTrace, LogType type) {
 
-        if (allLoggedText[allLoggedText.Count-1].LogMessage==text) {
-            allLoggedText[allLoggedText.Count-1].LogCount++;
-        } else {
-            allLoggedText.Add(new LoggedText(text, StackTraceUtility.ExtractStackTrace(), type));
-        }
+        try {
+            if (allLoggedText.Count>0&&allLoggedText[allLoggedText.Count-1].LogMessage==text) {
+                allLoggedText[allLoggedText.Count-1].SetMessageNumber(allLoggedText[allLoggedText.Count-1].LogCount+1);
+            } else {
 
+                SGroup logGroup = new SGroup {
+                    AutoLayout=(SGroup g) => g.AutoLayoutRows,
+                    AutoGrowDirection=SGroup.EDirection.Both,
+                    Children={
+                    new SButton(text) { Background=new Color(0,0,0,0) },
+                    new SLabel (stackTrace) {Visible=false},
+                }
+                };
+
+                LoggedText newText = new LoggedText(text, StackTraceUtility.ExtractStackTrace(), type) {
+                    MessageLabel=(SButton)logGroup.Children[0],
+                    StacktraceLabel=(SLabel)logGroup.Children[1]
+                };
+
+                newText.Start();
+
+                //Instance.GUI.Children.Add(logGroup);
+            }
+        }
+        catch (System.Exception e) {
+            Instance.GUI.Children.Add(new SLabel(e.ToString()));
+        }
     }
 
     class LoggedText {
 
-        public LoggedText(string logMessage,string stackTrace, LogType type) {
-            this.LogMessage=logMessage;
+        public LoggedText(string logMessage, string stackTrace, LogType type) {
             this.Stacktace=stackTrace;
             this.LogType=type;
         }
 
         public string LogMessage;
         public string Stacktace;
-        public bool IsStacktraceShown=false;
-        public int LogCount=0;
+        public bool IsStacktraceShown = false;
+        public int LogCount = 0;
         public LogType LogType;
+
+        public SButton MessageLabel;
+        public SLabel StacktraceLabel;
+
+        public void Start() {
+            MessageLabel.OnClick+=OnClick;
+        }
+
+        void OnClick(SButton button) {
+            IsStacktraceShown=!IsStacktraceShown;
+            StacktraceLabel.Visible=IsStacktraceShown;
+        }
+
+        public void SetMessageNumber(int number) {
+            if (number!=0) {
+                MessageLabel.Text=number.ToString()+LogMessage;
+            } else {
+                MessageLabel.Text=LogMessage;
+            }
+
+            LogCount=number;
+        }
     }
 
 }
-
