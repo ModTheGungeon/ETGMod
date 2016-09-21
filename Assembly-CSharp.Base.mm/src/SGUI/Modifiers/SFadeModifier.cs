8fa2bb37490af962bef55453d25f388fbf3401d3
Maik Macho <nuda1998@gmail.com>
2016-09-21 14:59:35 +0200

    Much SGUI

---
+++ Assembly-CSharp.Base.mm/src/SGUI/Modifiers/SFadeModifier.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+
+namespace SGUI {
+    public class SFadeOutModifier : STimedModifier {
+
+        public SFadeOutModifier()
+            : this(0.3f) {
+        }
+
+        public SFadeOutModifier(float duration)
+            : base(duration) {
+            Duration = duration;
+        }
+
+        public Color OrigForeground;
+        public Color OrigBackground;
+
+        public Color TransparentForeground;
+        public Color TransparentBackground;
+
+        public override void OnStart() {
+            OrigForeground = Elem.Foreground;
+            OrigBackground = Elem.Background;
+        }
+
+        public override void Animate(float t) {
+            Elem.Foreground = OrigForeground.WithAlpha(t * OrigForeground.a);
+            Elem.Background = OrigBackground.WithAlpha(t * OrigBackground.a);
+        }
+
+    }
+}
