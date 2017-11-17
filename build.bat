03bf6e5a22c9a6abf55bf933d58d24750c2d9703
Zatherz <zatherz@linux.pl>
2017-11-17 18:31:53 +0100

    Functional Windows build.bat file

--- build.bat
+++ build.bat
@@ -1,34 +1,79 @@
 @echo off
+setlocal ENABLEDELAYEDEXPANSION
 
+:: Requirements
+where xbuild >nul 2>nul
+if not %errorlevel%==0 (
+  where msbuild >nul 2>nul
+  if not %errorlevel%==0 (
+    echo ERROR: Neither xbuild nor msbuild were found. Make sure you have Mono or .NET Framework installed and the binaries are available in the PATH.
+    goto _exit
+  )
+)
 
+set sevenz=7z
+if exist C:\Program Files\7-Zip\7z.exe (
+  set sevenz="C:\Program Files\7-Zip\7z.exe"
+) else (
+  where 7z >nul 2>nul
+  if not %errorlevel%==0 (
+    echo ERROR: 7zip was not found. Make sure that you have it installed and in the PATH.
+    goto _exit
+  )
+)
+
+:: Prepare the target
 set target=Debug
-
 if %1.==release. goto _if_setrelease
 goto _ifj_setrelease
 :_if_setrelease
 set target=Release
 :_ifj_setrelease
 
+:: Prepare the build directory
 set build_base=build
 set build_etgmod=ETGMOD
-set build=%build_base%/%build_etgmod%
-set build_zip=%build_base%/%build_etgmod%.zip
+set "build=%build_base%\%build_etgmod%"
+set "build_zip=%build_base%\%build_etgmod%.zip"
 
-rmdir /q /s %build_base%
-mkdir %build_base%
+if exist "%build_base%" rmdir /q /s "%build_base%"
+mkdir "%build_base%" 2>nul
+mkdir "%build%" 2>nul
 
-:: msbuild.exe
-
-echo a
-for /f "tokens=*" %%a in (build-files) do (
-  echo line=%%a
+:: Build
+where xbuild >nul 2>nul
+if %errorlevel%==0 (
+  ::call xbuild
+  rem
+) else (
+  call msbuild
 )
+
+for /f "tokens=*" %%L in (build-files) do (
+  set "line=%%L"
+  set "line=!line:/=\!"
+  if not "!line:~0,1!"=="#" (
+    set "file=!line:{TARGET}=%target%!"
+    for %%f in (!file!) do set target=%%~nxf
+      rem
+
+    echo Copying '!file!' to '%build%/!target!'
+
+    for %%i in (!file!) do (
+      if exist %%~si\nul (
+        robocopy "!file!" "%build%/!target!" /s /e
+      ) else (
+        copy "!file!" "%build%/!target!"
+      )
+    )
+  )
 )
 
-for /F "tokens=*" %%L in (build-files) do (
-  echo %%L
-  if /i "%result:~0,1%"=="#" goto _continue_eachline
-  echo copying %%L
-  xcopy /E %%L %build%/%%L
-  :_continue_eachline
-)
+:: Zipping it all up
+pushd "%build%"
+%sevenz% a ETGMOD.zip *
+popd
+move "%build%\ETGMOD.zip" "%build_zip%"
+
+:: The End
+:_exit
